519d7a203f374b6430b3cb5f7f1e89d7c6c5152d
KAILASHRATHORE1997 <kailashrathore551997@gmail.com>
2018-03-13 01:18:04 +0530

    kailash

--- app/src/main/java/com/example/popla/gorkhavidyutpowerpay/Power.java
+++ app/src/main/java/com/example/popla/gorkhavidyutpowerpay/Power.java
@@ -9,5 +9,7 @@ public class Power extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power);
+        int c;
+        String koml;
     }
 }
