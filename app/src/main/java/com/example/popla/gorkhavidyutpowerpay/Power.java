fe484e4980c12e6310d73c2dc0beebff4d5a6c19
popla <komalkhatri03021996@gmail.com>
2018-03-14 17:04:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/popla/gorkhavidyutpowerpay/Power.java
@@ -0,0 +1,13 @@
+package com.example.popla.gorkhavidyutpowerpay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Power extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_power);
+    }
+}
