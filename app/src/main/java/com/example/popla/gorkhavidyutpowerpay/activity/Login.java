917afcd513f68453e544dc1bb6f20dea599d839f
Mohit Choudhary <choudharymohit1109@gmail.com>
2018-03-17 15:02:26 +0530

    Implement Green dao

---
+++ app/src/main/java/com/example/popla/gorkhavidyutpowerpay/activity/Login.java
@@ -0,0 +1,59 @@
+package com.example.popla.gorkhavidyutpowerpay.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.popla.gorkhavidyutpowerpay.AppController;
+import com.example.popla.gorkhavidyutpowerpay.Power;
+import com.example.popla.gorkhavidyutpowerpay.R;
+import com.example.popla.gorkhavidyutpowerpay.SignUp;
+import com.example.popla.gorkhavidyutpowerpay.db.DaoSession;
+import com.example.popla.gorkhavidyutpowerpay.db.User;
+
+import java.util.List;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        insertDataIntoDb();
+        getUserDATA();
+    }
+
+    private void getUserDATA() {
+        DaoSession daoSession = ((AppController) getApplication()).getDaoSession();
+        List<User> list = daoSession.getUserDao().loadAll();
+        Log.i("RESULT", list.get(0).getEmail());
+        Toast.makeText(this,  list.get(0).getEmail().toString(), Toast.LENGTH_LONG).show();
+    }
+
+    private void insertDataIntoDb() {
+
+        DaoSession daoSession = ((AppController) getApplication()).getDaoSession();
+        User user=new User();
+        user.setEmail("dfkjdhkjds");
+        user.setFirst_name("dfjkdfhkdf");
+        user.setLast_name("dfjkdfhkdf");
+        user.setUser_id(101);
+        daoSession.getUserDao().insert(user);
+    }
+
+    Intent intent;
+    public void SignIn(View view)
+    {
+        intent =new Intent(this,Power.class);
+        startActivity(intent);
+    }
+    public void sign(View view)
+    {
+        intent = new Intent(this,SignUp.class);
+        startActivity(intent);
+    }
+}
---
+++ greendaolibrary/src/main/java/com/example/greendaolibrary/MyGenerator.java
@@ -0,0 +1,44 @@
+package com.example.greendaolibrary;
+
+import org.greenrobot.greendao.generator.DaoGenerator;
+import org.greenrobot.greendao.generator.Entity;
+import org.greenrobot.greendao.generator.Schema;
+
+public class MyGenerator {
+    public static void main(String[] args) {
+        Schema schema = new Schema(1, "com.example.popla.gorkhavidyutpowerpay.db"); // Your app package name and the (.db) is the folder where the DAO files will be generated into.
+        schema.enableKeepSectionsByDefault();
+
+        addTables(schema);
+
+        try {
+            new DaoGenerator().generateAll(schema,"./app/src/main/java");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addTables(final Schema schema) {
+        addUserEntities(schema);
+       // addPhonesEntities(schema);
+    }
+
+    // This is use to describe the colums of your table
+    private static Entity addUserEntities(final Schema schema) {
+        Entity user = schema.addEntity("User");
+        user.addIdProperty().primaryKey().autoincrement();
+        user.addIntProperty("user_id").notNull();
+        user.addStringProperty("last_name");
+        user.addStringProperty("first_name");
+        user.addStringProperty("email");
+        return user;
+    }
+    
+ //    private static Entity addPhonesEntities(final Schema schema) {
+ //        Entity phone = schema.addEntity("Phone");
+ //        phone.addIdProperty().primaryKey().autoincrement();
+ //        phone.addIntProperty("user_id").notNull();
+ //        phone.addStringProperty("number");
+ //        return phone;
+ //    }
+}
