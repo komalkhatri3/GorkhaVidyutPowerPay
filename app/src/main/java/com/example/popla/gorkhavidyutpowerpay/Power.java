a4d049b241193ef534a786efca276cef0dbfa419
DELL <divya.kalal06@gmail.com>
2018-03-18 23:29:47 -0700

    stash

--- app/src/main/java/com/example/popla/gorkhavidyutpowerpay/Power.java
+++ app/src/main/java/com/example/popla/gorkhavidyutpowerpay/Power.java
@@ -9,8 +9,6 @@ public class  Power extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power);
-        int c;
-        String koml;
 
     }
 
