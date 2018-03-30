4409514d093b949be8aced832f19dd6695fc2349
deepeshalways <deepeshalways@gmail.com>
2018-03-30 22:14:19 +0530

    mergeing

--- app/src/main/java/com/example/popla/gorkhavidyutpowerpay/activity/Login.java
+++ app/src/main/java/com/example/popla/gorkhavidyutpowerpay/activity/Login.java
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+
 import com.example.popla.gorkhavidyutpowerpay.R;
 import com.example.popla.gorkhavidyutpowerpay.activity.Verifier.VerifierNewConnection;
 
@@ -24,12 +25,13 @@ public class Login extends AppCompatActivity {
 
      EditText emailid,pass;
     CheckBox cb1;
-    Button login;
+
     String username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+<<<<<<< Updated upstream
         emailid =(EditText) findViewById(R.id.UserName) ;
         final TextView textView=(TextView)findViewById(R.id.textView);
        pass = (EditText)findViewById(R.id.password);
@@ -55,6 +57,29 @@ public class Login extends AppCompatActivity {
                 register();
             }
         });
+=======
+        emailid = (EditText) findViewById(R.id.UserName);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        pass = (EditText) findViewById(R.id.password);
+        cb1 = (CheckBox) findViewById(R.id.checkbox1);
+
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (!isChecked) {
+                    pass.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                } else
+                    pass.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }
+        });
+    }
+
+   /* private void getUserDATA() {
+        DaoSession daoSession = ((AppController) getApplication()).getDaoSession();
+        List<User> list = daoSession.getUserDao().loadAll();
+        Log.i("RESULT", list.get(0).getEmail());
+        Toast.makeText(this,  list.get(0).getEmail().toString(),Toast.LENGTH_LONG).show();
+>>>>>>> Stashed changes
     }
     public void register()
     {
@@ -62,6 +87,7 @@ public class Login extends AppCompatActivity {
         if(!validate())
         {
 
+<<<<<<< Updated upstream
         }
         else
         {
@@ -70,6 +96,34 @@ public class Login extends AppCompatActivity {
 
     }
     public void onLogInSuccess()
+=======
+    private void insertDataIntoDb() {
+        DaoSession daoSession = ((AppController) getApplication()).getDaoSession();
+        User user=new User();
+        user.setEmail("dfkjdhkjds");
+        user.setFirst_name("dfjkdfhkdf");
+        user.setLast_name("dfjkdfhkdf");
+        user.setUser_id(101);
+        daoSession.getUserDao().insert(user);
+    }
+
+   public void register()
+   {
+       intialize();
+       if(!validate())
+       {
+
+       }
+
+   }
+   */
+   public  void intialize() {
+       username = emailid.getText().toString().trim();
+       password = pass.getText().toString().trim();
+   }
+
+    public void LogIn(View view)
+>>>>>>> Stashed changes
     {
         //after validate button Click Content
         Intent intent;  int count=0;
